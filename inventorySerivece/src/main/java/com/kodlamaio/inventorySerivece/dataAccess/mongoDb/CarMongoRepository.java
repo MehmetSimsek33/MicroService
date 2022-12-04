f49ecf0c7cf8b57e8ee6735f04e0212e11403bb2
Mehmet Simsek <mehmetsimsek3212368@hotmail.com>
2022-12-05 00:50:10 +0300

    trigger

--- inventorySerivece/src/main/java/com/kodlamaio/inventorySerivece/dataAccess/mongoDb/CarMongoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kodlamaio.inventorySerivece.dataAccess.mongoDb;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.kodlamaio.inventorySerivece.entities.mongo.CarMongoDetail;
-
-public interface CarMongoRepository extends MongoRepository<CarMongoDetail, String> {
-
-}
