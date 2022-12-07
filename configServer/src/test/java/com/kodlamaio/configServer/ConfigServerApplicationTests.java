faf4ec58ed57bd4971cad9403f73fe547dee7b87
Mehmet Simsek <mehmetsimsek3212368@hotmail.com>
2022-12-07 11:48:55 +0300

    config server

--- configServer/src/test/java/com/kodlamaio/configServer/ConfigServerApplicationTests.java
+++ configServer/src/test/java/com/kodlamaio/configServer/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.kodlamaio.configservice;
+package com.kodlamaio.configServer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServiceApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
